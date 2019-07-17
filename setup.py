af89a242cb335548ac2671349d94092af46b35dc
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-07-17 09:33:18 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name='organizador-de-arquivos',
+    description='Organizador de Arquivos.',
+    url='https://github.com/jjoaovitor7/organizador-de-arquivos',
+    packages=['src']
+)
